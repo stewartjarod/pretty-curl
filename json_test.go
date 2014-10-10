879c1d3a10a1b07b6f460e8705701059a29eaa8c
Jarod Stewart <pair+jarod.stewart@sendgrid.com>
2014-10-10 16:23:02 -0600

    add json test

---
+++ json_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+var testCases = []struct {
+	jsonString string
+	expected   bool
+}{
+	{"{ \"JSON\": 1, \"Test\": true}", true},
+	{"{ \"JSON\": 1 }", true},
+	{"{ JSON: 1 }", false},
+	{"2400", false},
+}
+
+func TestJSON(t *testing.T) {
+	for _, test := range testCases {
+		observed := IsJSON(test.jsonString)
+
+		if observed != test.expected {
+			t.Fatalf("%s returned %v but should be %v", test.jsonString, observed, test.expected)
+		}
+	}
+}
